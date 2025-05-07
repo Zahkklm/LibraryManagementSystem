0de09040a8814615f0df05b4c7f0a0b42ea8835f
Özgür Peynirci <ozgurpeynirci@gmail.com>
2025-05-07 19:13:26 +0300

    Removed Discovery Service tests as discovery service configuration is modified and tests do not fit with new changes.

--- discovery-service/src/main/java/com/librarysystem/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/librarysystem/discoveryservice/DiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.librarysystem.discovery;
+package com.librarysystem.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,3 +11,4 @@ public class DiscoveryServiceApplication {
     SpringApplication.run(DiscoveryServiceApplication.class, args);
   }
 }
+
--- discovery-service/src/test/java/com/librarysystem/discoveryservice/DiscoveryServiceApplicationTests.java
+++ discovery-service/src/test/java/com/librarysystem/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.librarysystem.discovery;
+package com.librarysystem.discoveryservice;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
