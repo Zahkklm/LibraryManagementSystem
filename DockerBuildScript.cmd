4ffcd1d219944b8ef46ec85ea88ff1c97560b40f
Özgür Peynirci <ozgurpeynirci@gmail.com>
2025-05-13 04:30:06 +0300

    Added docker build scripts for Windows and macOS/Linux

---
+++ DockerBuildScript.cmd
@@ -0,0 +1,6 @@
+docker build -t user-service ./user-service
+docker build -t book-service ./book-service
+docker build -t borrow-service ./borrow-service
+docker build -t auth-service ./auth-service
+docker build -t api-gateway ./api-gateway
+docker build -t discovery-service ./discovery-service
